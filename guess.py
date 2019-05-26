a143346fbd168b2bcc85c8ee0a11bbccbde20b78
ranjana priya <ranipriyap803@gmail.com>
2019-05-26 15:25:30 +0530

    Change the range from 20 to 30

--- guess.py
+++ guess.py
@@ -6,8 +6,8 @@ guessesTaken = 0
 print('Hello! What is your name?')
 myName = input()
 
-number = random.randint(1, 20)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+number = random.randint(1, 30)
+print('Well, ' + myName + ', I am thinking of a number between 1 and 30.')
 
 for guessesTaken in range(6):
     print('Take a guess.') 
